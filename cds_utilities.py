4878844d9a2c45f390ce5abd7e94afc093e62578
jenni prokkola <jmprok@utu.fi>
2015-04-21 14:59:39 +0300

    update of the cds_utilities script

--- cds_utilities.py
+++ cds_utilities.py
@@ -1,8 +1,9 @@
 #Coding sequences utilities
 
 def test_if_seq_begins_start_codon(sequence):
-	if sequence[0:3]== "ATG":
+	if sequence.upper()[0:3]== "ATG":
 		return("CorrectStart")
 	else:
 		return("wrongStart")
+
 
